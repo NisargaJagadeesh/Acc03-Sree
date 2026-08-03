54947a9e2ab36c878c0f3d4c368b02e37d976014
NisargaJagadeesh <nisarganisarga011@gmail.com>
2026-08-03 19:42:24 +0530

    Add files via upload
    Second

--- Main.java
+++ Main.java
@@ -1,36 +1,49 @@
-package com.tnsif.collectionframework;
+package com.tnsif.driver;
 
-import java.util.Stack;
+import java.util.Scanner;
+import com.tnsif.assignment1.TicketBooking;
 
-//demo for stack
+public class Main {
 
-public class Stackdemo {
-	
-	public static void main(String[] args) {
-		Stack<String> books=new Stack<>();
-		
-		//add the data push()
-		
-		books.push("java");
-		books.push("python");
-		books.push("react");
-		books.add("java");
-		books.push("python");
-		books.push("react");
-		System.out.println(books);
-		
-		//peek()
-		System.out.println("top book "+books.peek());
-		
-		//search()
-		System.out.println(books.search("java"));
-		
-		//pop()
-		System.out.println(books.pop());
-		
-		System.out.println(books);
-		
-		System.out.println();
-	}
+    public static void main(String[] args) {
 
+        Scanner sc = new Scanner(System.in);
+
+        String input = sc.nextLine();
+        String[] data = input.split(",");
+
+        TicketBooking ticket = new TicketBooking(
+                data[0],
+                data[1],
+                Integer.parseInt(data[2]));
+
+        int choice = Integer.parseInt(sc.nextLine());
+
+        switch (choice) {
+
+        case 1:
+            Double amount = Double.parseDouble(sc.nextLine());
+            ticket.makePayment(amount);
+            break;
+
+        case 2:
+            Double walletAmount = Double.parseDouble(sc.nextLine());
+            String walletNumber = sc.nextLine();
+            ticket.makePayment(walletNumber, walletAmount);
+            break;
+
+        case 3:
+            String holderName = sc.nextLine();
+            Double cardAmount = Double.parseDouble(sc.nextLine());
+            String creditCard = sc.nextLine();
+            String ccv = sc.nextLine();
+            ticket.makePayment(creditCard, ccv, holderName, cardAmount);
+            break;
+
+        default:
+            System.out.println("Invalid choice");
+        }
+
+        sc.close();
+    }
 }
